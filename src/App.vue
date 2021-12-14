3cb40595cabbae04a996029908f3eb39bd40e6fc
codwerk <louisws@outlook.fr>
2021-12-14 18:29:29 +0100

    Header & Tailwind Config

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+import Header from './components/Header.vue'
+
+ export default {
+  components: {
+    Header 
+  }
+ }
+</script>
