a686c55b9fd0d9e4a0ef652791ae81f18d38cad8
codwerk <louisws@outlook.fr>
2021-12-16 01:09:07 +0100

    API Composition

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -38,9 +38,11 @@
   export default {
     name: 'DataBoxes',
     props: ['stats'],
-    methods: {
-      numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    setup() {
+      return {
+        numberWithCommas(x) {
+          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        }
       }
     }
   }
